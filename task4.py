32687f24d3a89cd906867a7e7e6c5a9210d0044b
samruddhidhumal193 <samruddhidhumal193@gmail.com>
2025-12-14 20:44:14 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,21 @@
+def chatbot_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hi!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+    elif user_input == "bye":
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand."
+
+print("🤖 Chatbot: Hello! Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ")
+    response = chatbot_response(user_input)
+    print("🤖 Chatbot:", response)
+
+    if user_input.lower() == "bye":
+        break
